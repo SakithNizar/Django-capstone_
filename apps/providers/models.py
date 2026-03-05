6ce33e5fede8e1c4c4e07a1a3cdb3c0cd80e65b2
SakithNizar <sakithmohamed54@gmail.com>
2026-03-05 13:17:21 +0530

    feat: add provider domain model

--- apps/providers/models.py
+++ apps/providers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=255)
+    service_type = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
